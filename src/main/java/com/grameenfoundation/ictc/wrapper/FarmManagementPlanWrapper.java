a4020e96f35358e0195d76a4c7b211db04d294cb
Joseph George Davis <>
2015-08-07 13:00:06 +0000

    Farm management plan domain

---
+++ src/main/java/com/grameenfoundation/ictc/wrapper/FarmManagementPlanWrapper.java
@@ -0,0 +1,185 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.grameenfoundation.ictc.wrapper;
+
+/**
+ *
+ * @author Joseph George Davis
+ * @date Aug 7, 2015 10:52:24 AM
+ * description:
+ */
+public class FarmManagementPlanWrapper {
+    
+ String dateoflandidentification="dateoflandidentification"; 
+ String locationofland="locationofland";
+ String targetareaofland="targetareaofland";
+ String costofrentingland="costofrentingland";
+ String targetyieldperacre="targetyieldperacre";
+ String targetproduction="targetproduction";
+ String expectedpriceperton="expectedpriceperton";
+ String nameofvariety="nameofvariety";
+ String purchaseofstakesdate="purchaseofstakesdate";
+ String quantityofplantingmaterial="quantityofplantingmaterial";
+ String weightofplantingmaterialperacre="weightofplantingmaterialperacre";
+ String quantityofplantingmaterialperacre="quantityofplantingmaterialperacre";
+ String costperunitofplantingmaterial="costperunitofplantingmaterial";
+ String methodoflandclearing="methodoflandclearing";
+ String typeofherbicideforpreplanting="typeofherbicideforpreplanting";
+ String quantityOfherbicide="quantityofherbicide";
+ String purchaseofherbicidedate="purchaseofherbicidedate";
+ 
+ 
+ public  void setPurchaseofherbicide(String purchaseofherbicide) {
+      this.purchaseofherbicidedate=purchaseofherbicide;
+    }
+ public  String getPurchaseofherbicide() {
+        return purchaseofherbicidedate;
+
+    }
+ 
+ public  void setQuantityOfherbicide(String quantityOfherbicide) {
+      this.quantityOfherbicide=quantityOfherbicide;
+    }
+ public  String getQuantityOfherbicide() {
+        return quantityOfherbicide;
+
+    }
+ 
+ 
+ public  void setTypeofherbicideforpreplanting(String typeofherbicideforpreplanting) {
+      this.typeofherbicideforpreplanting=typeofherbicideforpreplanting;
+    }
+ public  String getTypeofherbicideforpreplanting() {
+        return typeofherbicideforpreplanting;
+
+    }
+ public  void setMethodoflandclearing(String methodoflandclearing) {
+      this.methodoflandclearing=methodoflandclearing;
+    }
+ public  String getMethodoflandclearing() {
+        return methodoflandclearing;
+
+    }
+ 
+ 
+ public  void setCostperunitofplantingmaterial(String costperunitofplantingmaterial) {
+      this.costperunitofplantingmaterial=costperunitofplantingmaterial;
+    }
+ public  String getCostperunitofplantingmaterial() {
+        return costperunitofplantingmaterial;
+
+    }
+ 
+ public  void setQuantityofplantingmaterialperacre(String quantityofplantingmaterialperacre) {
+      this.quantityofplantingmaterialperacre=quantityofplantingmaterialperacre;
+    }
+ public  String getQuantityofplantingmaterialperacre() {
+        return quantityofplantingmaterialperacre;
+
+    }
+ 
+ public  void setWeightofplantingmaterialperacre(String weightofplantingmaterialperacre) {
+      this.weightofplantingmaterialperacre=weightofplantingmaterialperacre;
+    }
+ public  String getWeightofplantingmaterialperacre() {
+        return weightofplantingmaterialperacre;
+
+    }
+ 
+ public  void setQuantityofplantingmaterial(String quantityofplantingmaterial) {
+      this.quantityofplantingmaterial=quantityofplantingmaterial;
+    }
+ public  String getQuantityofplantingmaterial() {
+        return quantityofplantingmaterial;
+
+    }
+ 
+ public  void setPurchaseofstakesdate(String purchaseofstakesdate) {
+      this.purchaseofstakesdate=purchaseofstakesdate;
+    }
+ public  String getPurchaseofstakesdate() {
+        return purchaseofstakesdate;
+
+    }
+ 
+ 
+ public  void setNameofvariety(String nameofvariety) {
+      this.nameofvariety=nameofvariety;
+    }
+ public  String getNameofvariety() {
+        return nameofvariety;
+
+    }
+ 
+ 
+ public  void setExpectedpriceperton(String expectedpriceperton) {
+      this.expectedpriceperton=expectedpriceperton;
+    }
+ public  String getExpectedpriceperton() {
+        return expectedpriceperton;
+
+    }
+ 
+ public  void setTargetproduction(String targetproduction) {
+      this.targetproduction=targetproduction;
+    }
+ public  String getTargetproduction() {
+        return targetproduction;
+
+    }
+ 
+ public  void setTargetyieldperacre(String targetyieldperacre) {
+      this.targetyieldperacre=targetyieldperacre;
+    }
+ public  String getTargetyieldperacre() {
+        return targetyieldperacre;
+
+    }
+ 
+ 
+ public  void setCostofrentingland(String costofrentingland) {
+      this.costofrentingland=costofrentingland;
+    }
+ public  String getCostofrentingland() {
+        return costofrentingland;
+
+    }
+ 
+ 
+ public  void setTargetareaofland(String targetareaofland) {
+      this.targetareaofland=targetareaofland;
+    }
+ public  String getTargetareaofland() {
+        return targetareaofland;
+
+    }
+ 
+ 
+ 
+ 
+ public  void setLocationofland(String locationofland) {
+      this.locationofland=locationofland;
+    }
+ public  String getLocationofland() {
+        return locationofland;
+
+    }
+ 
+ 
+ 
+ 
+  public  void setDateoflandidentification(String dateoflandidentification) {
+      this.dateoflandidentification=dateoflandidentification;
+    }
+ public  String getDateoflandidentification() {
+        return dateoflandidentification;
+
+    }
+ 
+ 
+
+}
