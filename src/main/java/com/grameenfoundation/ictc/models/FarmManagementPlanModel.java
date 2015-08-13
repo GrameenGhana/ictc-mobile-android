580e3822902e11c7940dd012091d5d36e77865bb
Joseph George Davis <>
2015-08-13 11:55:39 +0000

    Farm management plan domain

---
+++ src/main/java/com/grameenfoundation/ictc/models/FarmManagementPlanModel.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.grameenfoundation.ictc.models;
+
+/**
+ *
+ * @author Joseph George Davis
+ * @date Aug 13, 2015 10:25:01 AM
+ * description:
+ */
+public class FarmManagementPlanModel {
+    
+    
+    
+    
+
+}
